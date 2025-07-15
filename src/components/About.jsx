d1166afbb6ec6dae03394eea39cb946d9fb59d5a
Ethan Lum <ethan_lum@sfu.ca>
2025-07-14 17:41:34 -0700

    bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -40,11 +40,11 @@ const About = () => {
         variants={fadeIn('', '', 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I am a Computer Science student at SRM University and multilingual speaker with a passion
-        for ethical hacking, artificial intelligence, and neural language processing. I am looking
-        to join a company that values innovation, collaboration, and continuous learning and am
-        confident that my enthusiasm and drive will help me succeed in any role that leverages my
-        passion for technology.
+        I'm currently pursuing a Bachelor of Science in Interactive Arts and Technology at Simon Fraser University, 
+        specializing in user experience and interface (UX/UI) development, artificial intelligence, and sound design. 
+        My passion for music production and innovative audio solutions drives me to blend creativity with cutting-edge technology, 
+        delivering immersive experiences and intuitive interfaces. I'm eager to collaborate with forward-thinking companies, 
+        contributing my multidisciplinary skill set and creative vision to impactful projects.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
