b2efa2b70553f2d7525405488276991d4e32ac35
Ethan Lum <ethan_lum@sfu.ca>
2025-07-15 19:39:17 -0700

    3d model update

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -28,7 +28,7 @@ const ComputerModel = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={scene}
-        scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 1.4 : 1.5} 
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
