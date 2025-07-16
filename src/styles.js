7a2b77d7ff78f869f5c55b58731bcf14e1674b11
Ethan Lum <ethan_lum@sfu.ca>
2025-07-16 01:36:17 -0700

    fix mobile

--- src/styles.js
+++ src/styles.js
@@ -2,8 +2,10 @@ const styles = {
   paddingX: "sm:px-16 px-6",
  // paddingY: "sm:py-16 py-6",
  //padding: "sm:px-16 px-6 sm:py-16 py-10",
-  paddingY: "sm:py-16 py-6",               // mobile → py-6 instead of py-10
-  padding:  "sm:px-16 px-6 sm:py-16 py-6",  // ditto here
+  //paddingY: "sm:py-16 py-6",               // mobile → py-6 instead of py-10
+  //padding:  "sm:px-16 px-6 sm:py-16 py-6",  // ditto here
+  paddingY: "md:py-16 py-6",
+  padding:  "md:px-16 px-6 md:py-16 py-6",
 
   heroHeadText:
     "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
