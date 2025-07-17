88d09042143f19e8fb7a3bec7e0ebb3e31b8e78e
Ethan Lum <ethan_lum@sfu.ca>
2025-07-17 12:10:16 -0700

    remove logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -61,7 +61,7 @@ const Navbar = () => {
               window.scrollTo(0, 0);
             }}
           >
-            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            
             <p className="text-white text-[20px] font-bold cursor-pointer flex">
               ETHAN&nbsp;
               <span className="sm:block hidden">LUM</span>
